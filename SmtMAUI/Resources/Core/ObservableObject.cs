65624b968060191da65f7a22cf3a2f7193456643
Edn.Santos <135707128+EdBLK@users.noreply.github.com>
2024-11-23 08:54:29 -0300

    Update 1

---
+++ SmtMAUI/Resources/Core/ObservableObject.cs
@@ -0,0 +1,22 @@
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace ModernDesign.Core
+{
+    class ObservableObject : INotifyPropertyChanged{
+
+         public event PropertyChangedEventHandler PropertyChanged;
+
+          protected void onPropertyChanged([CallerMemberName] string name = null)
+        {
+            onPropertyChanged();
+        }
+    }
+   
+
+
+    
+
+}
